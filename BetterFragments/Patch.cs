fef8162ce6efb922f91ad8e0046538ba13d27270
Gustavo Ribeiro <pokegustavo@gmail.com>
2023-05-12 21:15:19 -0300

    descriptions should also go back to normal when mod is off

--- BetterFragments/Patch.cs
+++ BetterFragments/Patch.cs
@@ -9,61 +9,122 @@ namespace BetterFragments
         {
             static void Postfix(int fragID, out string fragName, out string fragDesc) 
             {
-                switch (fragID)
+				if (Mod.MasterHasMod)
 				{
-					case 0:
-						fragName = "A.O.G. Fragment";
-						fragDesc = "Your ship will become unflagged each jump\n+15% Credits from destroyed ships";
-						return;
-					case 1:
-						fragName = "Auction Fragment";
-						fragDesc = "Gain access to special deals in general stores across the galaxy";
-						return;
-					case 2:
-						fragName = "Colonial Union Fragment";
-						fragDesc = "+50% max shield integrity\n+15% warp range";
-						return;
-					case 3:
-						fragName = "High Roller Fragment";
-						fragDesc = "Random chance to either gain credits, processed scrap or coolant each jump";
-						return;
-					default:
-						fragName = "Fragment";
-						fragDesc = "";
-						return;
-					case 6:
-						fragName = "Fluffy Biscuit Fragment";
-						fragDesc = "2x fluffy oven food supply\n2x credits earned for biscuit sales";
-						return;
-					case 7:
-						fragName = "Grey Huntsmen Fragment";
-						fragDesc = "20% Player Health Boost\n10% turret damage";
-						return;
-					case 8:
-						fragName = "Warger's Fragment";
-						fragDesc = "+10 Weight capacity\n20% Speed boost";
-						return;
-					case 9:
-						fragName = "Cursed Fragment";
-						fragDesc = "A supposedly 'cursed' data fragment. If you believe in that sort of thing ...";
-						return;
-					case 10:
-						fragName = "Racing Fragment";
-						fragDesc = "+30% thrust";
-						return;
-					case 11:
-						fragName = "Commander Fragment";
-						fragDesc = "5% hull repair per jump";
-						return;
-					case 12:
-						fragName = "Cypher Fragment";
-						fragDesc = "Increases ship program slots to 12";
-						return;
-					case 13:
-						fragName = "W.D. Corp Fragment";
-						fragDesc = "Installed tracker missiles are refilled by 10% each jump\n+20% armor";
-						return;
+					switch (fragID)
+					{
+						case 0:
+							fragName = "A.O.G. Fragment";
+							fragDesc = "Your ship will become unflagged each jump\n+15% Credits from destroyed ships";
+							return;
+						case 1:
+							fragName = "Auction Fragment";
+							fragDesc = "Gain access to special deals in general stores across the galaxy";
+							return;
+						case 2:
+							fragName = "Colonial Union Fragment";
+							fragDesc = "+50% max shield integrity\n+15% warp range";
+							return;
+						case 3:
+							fragName = "High Roller Fragment";
+							fragDesc = "Random chance to either gain credits, processed scrap or coolant each jump";
+							return;
+						default:
+							fragName = "Fragment";
+							fragDesc = "";
+							return;
+						case 6:
+							fragName = "Fluffy Biscuit Fragment";
+							fragDesc = "2x fluffy oven food supply\n2x credits earned for biscuit sales";
+							return;
+						case 7:
+							fragName = "Grey Huntsmen Fragment";
+							fragDesc = "20% Player Health Boost\n10% turret damage";
+							return;
+						case 8:
+							fragName = "Warger's Fragment";
+							fragDesc = "+10 Weight capacity\n20% Speed boost";
+							return;
+						case 9:
+							fragName = "Cursed Fragment";
+							fragDesc = "A supposedly 'cursed' data fragment. If you believe in that sort of thing ...";
+							return;
+						case 10:
+							fragName = "Racing Fragment";
+							fragDesc = "+30% thrust";
+							return;
+						case 11:
+							fragName = "Commander Fragment";
+							fragDesc = "5% hull repair per jump";
+							return;
+						case 12:
+							fragName = "Cypher Fragment";
+							fragDesc = "Increases ship program slots to 12";
+							return;
+						case 13:
+							fragName = "W.D. Corp Fragment";
+							fragDesc = "Installed tracker missiles are refilled by 10% each jump\n+20% armor";
+							return;
+					}
 				}
+				else 
+				{
+                    switch (fragID)
+                    {
+                        case 0:
+                            fragName = "A.O.G. Fragment";
+                            fragDesc = "Your ship will become unflagged each jump";
+                            return;
+                        case 1:
+                            fragName = "Auction Fragment";
+                            fragDesc = "Gain access to special deals in general stores across the galaxy";
+                            return;
+                        case 2:
+                            fragName = "Colonial Union Fragment";
+                            fragDesc = "+50% max shield integrity\n+15% warp range";
+                            return;
+                        case 3:
+                            fragName = "High Roller Fragment";
+                            fragDesc = "Random chance to either gain credits, processed scrap or coolant each jump";
+                            return;
+                        default:
+                            fragName = "Fragment";
+                            fragDesc = "";
+                            return;
+                        case 6:
+                            fragName = "Fluffy Biscuit Fragment";
+                            fragDesc = "2x fluffy oven food supply\n2x credits earned for biscuit sales";
+                            return;
+                        case 7:
+                            fragName = "Grey Huntsmen Fragment";
+                            fragDesc = "No effect";
+                            return;
+                        case 8:
+                            fragName = "Warger's Fragment";
+                            fragDesc = "No effect";
+                            return;
+                        case 9:
+                            fragName = "Cursed Fragment";
+                            fragDesc = "A supposedly 'cursed' data fragment. If you believe in that sort of thing ...";
+                            return;
+                        case 10:
+                            fragName = "Racing Fragment";
+                            fragDesc = "+30% thrust";
+                            return;
+                        case 11:
+                            fragName = "Commander Fragment";
+                            fragDesc = "5% hull repair per jump";
+                            return;
+                        case 12:
+                            fragName = "Cypher Fragment";
+                            fragDesc = "Increases ship program slots to 12";
+                            return;
+                        case 13:
+                            fragName = "W.D. Corp Fragment";
+                            fragDesc = "Installed tracker missiles are refilled by 10% each jump";
+                            return;
+                    }
+                }
 			}
         }
 		[HarmonyPatch(typeof(PLShipStats), "CalculateStats")]
